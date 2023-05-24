8a3e322e3c5a7ae4e19cd2a284bbaae304be959c
Trévor Sénant <72136975+Tuturasimar@users.noreply.github.com>
2023-05-24 17:37:28 +0200

    Revert "Revert 8 seb entity user"

---
+++ Actiview/actiview-common-domain-model/src/main/java/fr/isika/cda/entities/users/UserData.java
@@ -0,0 +1,75 @@
+package fr.isika.cda.entities.users;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import fr.isika.cda.entities.common.JobEnum;
+
+@Entity
+
+public class UserData {
+
+	@Id
+	@GeneratedValue
+	private Long userId;
+	
+	private String lastname;
+	private String firstname;
+	private LocalDate birthday;
+	private String email;
+	
+	@Enumerated(EnumType.STRING)
+	private JobEnum jobEnum;
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public JobEnum getJobEnum() {
+		return jobEnum;
+	}
+
+	public void setJobEnum(JobEnum jobEnum) {
+		this.jobEnum = jobEnum;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+	
+	
+}

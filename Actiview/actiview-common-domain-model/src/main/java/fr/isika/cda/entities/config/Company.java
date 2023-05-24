ac789ca3b8816f7759cc77191ceef91cc43ab3dc
Diane REJA <diane.reja@gmail.com>
2023-05-24 15:14:12 +0200

    ajout classes Company et Contract dans package Config

---
+++ Actiview/actiview-common-domain-model/src/main/java/fr/isika/cda/entities/config/Company.java
@@ -0,0 +1,59 @@
+package fr.isika.cda.entities.config;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import fr.isika.cda.entities.common.StatusEnum;
+
+@Entity
+public class Company implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private long companyId;
+
+	private String name;
+	private String adress;
+	private StatusEnum status;
+
+	public long getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(long companyId) {
+		this.companyId = companyId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public StatusEnum getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusEnum status) {
+		this.status = status;
+	}
+
+}
---
+++ Actiview/actiview-common-domain-model/src/main/java/fr/isika/cda/entities/config/Contract.java
@@ -0,0 +1,97 @@
+package fr.isika.cda.entities.config;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import fr.isika.cda.entities.common.StatusEnum;
+
+@Entity
+public class Contract implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private long contractId;
+
+	private char label;
+	private float price;
+	private String description;
+	private StatusEnum status;
+
+	private LocalDate createdAt;
+	private LocalDate updatedAt;
+	private LocalDate canBeChangedAt;
+
+	public long getContractId() {
+		return contractId;
+	}
+
+	public void setContractId(long contractId) {
+		this.contractId = contractId;
+	}
+
+	public char getLabel() {
+		return label;
+	}
+
+	public void setLabel(char label) {
+		this.label = label;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public StatusEnum getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusEnum status) {
+		this.status = status;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDate getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDate updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public LocalDate getCanBeChangedAt() {
+		return canBeChangedAt;
+	}
+
+	public void setCanBeChangedAt(LocalDate canBeChangedAt) {
+		this.canBeChangedAt = canBeChangedAt;
+	}
+
+}

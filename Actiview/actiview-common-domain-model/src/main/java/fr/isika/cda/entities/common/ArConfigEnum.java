4d62e4a34c0defa72a88eadc90d7aa6ec03b731c
Diane REJA <diane.reja@gmail.com>
2023-05-24 15:18:43 +0200

    ajout classe Feature + Enum ArConfigEnum

---
+++ Actiview/actiview-common-domain-model/src/main/java/fr/isika/cda/entities/common/ArConfigEnum.java
@@ -0,0 +1,5 @@
+package fr.isika.cda.entities.common;
+
+public enum ArConfigEnum {
+
+}
---
+++ Actiview/actiview-common-domain-model/src/main/java/fr/isika/cda/entities/config/Feature.java
@@ -0,0 +1,68 @@
+package fr.isika.cda.entities.config;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import fr.isika.cda.entities.common.ArConfigEnum;
+
+@Entity
+public class Feature implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private long featureId;
+
+	private int nbOfCollaborators;
+	private ArConfigEnum arConfig;
+	private float price;
+	private int nbOfOptions;
+
+	public long getFeatureId() {
+		return featureId;
+	}
+
+	public void setFeatureId(long featureId) {
+		this.featureId = featureId;
+	}
+
+	public int getNbOfCollaborators() {
+		return nbOfCollaborators;
+	}
+
+	public void setNbOfCollaborators(int nbOfCollaborators) {
+		this.nbOfCollaborators = nbOfCollaborators;
+	}
+
+	public ArConfigEnum getArConfig() {
+		return arConfig;
+	}
+
+	public void setArConfig(ArConfigEnum arConfig) {
+		this.arConfig = arConfig;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getNbOfOptions() {
+		return nbOfOptions;
+	}
+
+	public void setNbOfOptions(int nbOfOptions) {
+		this.nbOfOptions = nbOfOptions;
+	}
+
+}

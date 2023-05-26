4550a7204b4a117bc12b2e4092e5f48cb7f36de7
Diane REJA <diane.reja@gmail.com>
2023-05-26 11:17:49 +0200

    Creation RegisterUser (bean)

--- Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/RegisterUser.java
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/RegisterUser.java
@@ -1,5 +1,36 @@
 package fr.isika.cda.beans;
 
-public class RegisterUser {
 
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+import fr.isika.cda.repository.UserRepository;
+import fr.isika.cda.viewmodels.UserViewModel;
+
+@ManagedBean
+public class RegisterUser {
+	
+	private UserViewModel userViewModel = new UserViewModel();
+	
+	@Inject
+	private UserRepository userRepo;
+
+	public void registerUser() {
+		Long id = userRepo.registerUser(userViewModel);
+		System.out.println("Id du user créé : "+ id);
+	}
+	
+	public void clear() {
+		userViewModel = new UserViewModel();
+	}
+
+	public UserViewModel getUserViewModel() {
+		return userViewModel;
+	}
+
+	public void setUserViewModel(UserViewModel userViewModel) {
+		this.userViewModel = userViewModel;
+	}
+	
+	
 }

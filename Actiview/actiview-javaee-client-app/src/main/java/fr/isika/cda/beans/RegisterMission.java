7028e4666792b729dbfb3b632835d2031dcd8ae7
Lagrange Laura <laura.lagrange74@gmail.com>
2023-05-26 10:58:43 +0200

    Creation Mission Form

---
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/RegisterMission.java
@@ -0,0 +1,9 @@
+package fr.isika.cda.beans;
+
+import fr.isika.cda.viewmodels.MissionViewModel;
+
+public class RegisterMission {
+	
+	private MissionViewModel missionVm = new MissionViewModel();
+
+}
---
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/viewmodels/MissionViewModel.java
@@ -0,0 +1,5 @@
+package fr.isika.cda.viewmodels;
+
+public class MissionViewModel {
+
+}

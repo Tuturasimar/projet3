7feca6756bfa2de51ff24bd4adb4e2529006a50d
arbam5 <arbam.saine@gmail.com>
2023-05-30 18:06:45 +0200

    login

---
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/viewmodels/LoginViewModel.java
@@ -0,0 +1,21 @@
+package fr.isika.cda.viewmodels;
+
+public class LoginViewModel {
+
+	private String password;
+	private String login;
+	
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+}

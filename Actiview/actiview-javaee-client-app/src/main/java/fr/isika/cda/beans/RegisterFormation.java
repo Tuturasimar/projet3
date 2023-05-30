6b1464308aba9f3e6694c9be3e3df5249a9bce6a
Sebastien MONTIEL <montiel.sebastien@gmail.com>
2023-05-30 18:40:30 +0200

    Formation register beans

---
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/RegisterFormation.java
@@ -0,0 +1,39 @@
+package fr.isika.cda.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+import fr.isika.cda.repository.FormationRepository;
+import fr.isika.cda.viewmodels.FormationViewModel;
+import fr.isika.cda.viewmodels.MissionViewModel;
+
+
+@ManagedBean
+public class RegisterFormation {
+
+	private FormationViewModel formationVm = new FormationViewModel();
+	@Inject
+	private FormationRepository formationRepository;
+	
+	public void register() {
+		Long id = formationRepository.register(formationVm);
+		System.out.println("La formation a bien été créée avec l'id: " + id);
+		clear();
+		
+	}
+	public void clear() {
+		formationVm = new FormationViewModel();
+	}
+	public void updateMission(Long id) {
+		System.out.println("La formation à mettre à jour est : " + id);
+	}
+
+	public FormationViewModel getFormationVm() {
+		return formationVm;
+	}
+
+	public void setFormationVm(FormationViewModel formationVm) {
+		this.formationVm = formationVm;
+	}
+	
+}
---
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/ShowFormationsBeans.java
@@ -0,0 +1,5 @@
+package fr.isika.cda.beans;
+
+public class ShowFormationsBeans {
+
+}

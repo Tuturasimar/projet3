ac5ed5f6889bd1307eae15493cbb58732ac8e063
Lagrange Laura <laura.lagrange74@gmail.com>
2023-05-31 18:01:29 +0200

    Save logo and banner in BDD

--- Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/FileUploadBean.java
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/FileUploadBean.java
@@ -1,31 +1,76 @@
 package fr.isika.cda.beans;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 
+import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.file.UploadedFile;
+
+import fr.isika.cda.repository.ImageConfigRepository;
+import fr.isika.cda.utils.FileUploadUtils;
+import fr.isika.cda.viewmodels.ImageConfigViewModel;
 
 @ManagedBean
 @SessionScoped
 public class FileUploadBean {
 
-	private UploadedFile file;
+	@Inject
+	private ImageConfigRepository imgConfigRepo;
 
-	public UploadedFile getFile() {
-		return file;
+	private ImageConfigViewModel imgConfigVm = new ImageConfigViewModel();
+	
+	private UploadedFile logo;
+	private UploadedFile banner;
+	
+	public String save() {
+		imgConfigRepo.save(imgConfigVm);
+		imgConfigVm = new ImageConfigViewModel();
+		return "adminPersonalizationPreview.xhtml";
+	}
+	
+	public void uploadLogo(FileUploadEvent e) {
+		String fileName = uploadFile(e);
+		// Mettre à jour le vm
+		imgConfigVm.setLogo(fileName);
+	}
+	public void uploadBanner(FileUploadEvent e) {
+		String fileName = uploadFile(e);
+		// Mettre à jour le vm
+		imgConfigVm.setBanner(fileName);
 	}
 
-	public void setFile(UploadedFile file) {
-		this.file = file;
+	public UploadedFile getBanner() {
+		return banner;
+	}
+	public void setBanner(UploadedFile banner) {
+		this.banner = banner;
+	}
+	public UploadedFile getLogo() {
+		return logo;
+	}
+	public void setLogo(UploadedFile logo) {
+		this.logo = logo;
 	}
 
-	public void upload() {
-		if (file != null) {
-			FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
-			FacesContext.getCurrentInstance().addMessage(null, message);
-		}
+	private String uploadFile(FileUploadEvent e) {
+		String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyyHHmmss"));
 
+		UploadedFile uploadedFile = e.getFile();
+		String fileName = String.join("_", timestamp, uploadedFile.getFileName());
+
+		// Ecrire le fichier physiquement qq part
+		FileUploadUtils.uploadFileToApp(uploadedFile, fileName);
+		
+		FacesMessage message = new FacesMessage("Successful file upload", uploadedFile.getFileName() + " is uploaded.");
+		FacesContext.getCurrentInstance().addMessage(null, message);
+		return fileName;
 	}
+	
+	
 }
---
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/viewmodels/ImageConfigViewModel.java
@@ -0,0 +1,24 @@
+package fr.isika.cda.viewmodels;
+
+public class ImageConfigViewModel {
+	
+	private String logo;
+	private String banner;
+	
+	public String getLogo() {
+		return logo;
+	}
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+	public String getBanner() {
+		return banner;
+	}
+	public void setBanner(String banner) {
+		this.banner = banner;
+	}
+	
+	
+	
+
+}

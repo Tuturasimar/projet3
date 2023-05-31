24ab9550418bc7c0357e16c874d8d20aa584d8e0
Lagrange Laura <laura.lagrange74@gmail.com>
2023-05-31 15:49:29 +0200

    Add button to upload a picture

---
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/FileUploadBean.java
@@ -0,0 +1,31 @@
+package fr.isika.cda.beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.model.file.UploadedFile;
+
+@ManagedBean
+@SessionScoped
+public class FileUploadBean {
+
+	private UploadedFile file;
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
+	public void upload() {
+		if (file != null) {
+			FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+
+	}
+}

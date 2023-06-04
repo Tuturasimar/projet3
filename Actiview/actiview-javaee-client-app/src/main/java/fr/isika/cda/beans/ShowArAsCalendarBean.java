cde09051da982f9702afba7849e1ab04d01e41b1
Diane REJA <diane.reja@gmail.com>
2023-06-04 21:51:58 +0200

    some cleaning in ShowArAsCalendar

--- Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/ShowArAsCalendarBean.java
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/ShowArAsCalendarBean.java
@@ -1,7 +1,5 @@
 package fr.isika.cda.beans;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,11 +9,11 @@ import javax.inject.Inject;
 
 import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.DefaultScheduleModel;
-import org.primefaces.model.ScheduleEvent;
 import org.primefaces.model.ScheduleModel;
 
 import fr.isika.cda.entities.ar.ActivityDate;
 import fr.isika.cda.entities.ar.Ar;
+import fr.isika.cda.entities.ar.PartDayEnum;
 import fr.isika.cda.repository.ActivityDateRepository;
 import fr.isika.cda.repository.ArRepository;
 import fr.isika.cda.viewmodels.ArCalendarViewModel;
@@ -34,54 +32,60 @@ public class ShowArAsCalendarBean {
 
 	private ScheduleModel calendar;
 
-//	@PostConstruct
-//    public void init() {
-//        calendar = new DefaultScheduleModel();
-//        Ar ar = arRepo.findById(4L);
-//        arCalendarVM.setCreatedAt(ar.getCreatedAt());
-//        arCalendarVM.setActivityDates(activityDateRepo.getAllActivityDateByArId(ar.getId()));
-//        DefaultScheduleEvent<?> event = DefaultScheduleEvent.builder()
-//            .title("Champions League Match")
-//            // as I tested, you NEED a start date and enddate
-//            .startDate(LocalDateTime.of(2023, 4, 13, 0, 0))
-//            .endDate(LocalDateTime.of(2023, 4, 15, 12, 0)) // change with tomorrow's date
-//            .build();
-//        calendar.addEvent(event);
-//    
-//        List<ActivityDate> activityDatesAsEvents = arCalendarVM.getActivityDates();
-//		for (ActivityDate activityDateAsEvent : activityDatesAsEvents) {
-//			DefaultScheduleEvent<?> event2 = DefaultScheduleEvent.builder()
-//					.title("test")
-//					.startDate(activityDateAsEvent.getDate().atTime(9, 0))
-//					.endDate(activityDateAsEvent.getDate().atTime(18, 0))
-//					.build();
-//			calendar.addEvent(event2);
-//		}
-//	}  
-
 	@PostConstruct
 	public void initTest() {
 		calendar = new DefaultScheduleModel();
 	}
 
+	/**
+	 * Méthode qui permet d'afficher un calendrier correspondant au mois du cra avec
+	 * toutes les activityDate qui lui sont liées
+	 * 
+	 * @param arId Id du cra
+	 */
 	public String showArCalendar(Long arId) {
+		// initialisation du calendrier vide
 		calendar = new DefaultScheduleModel();
 		Ar ar = arRepo.findById(arId);
 		arCalendarVM.setArId(ar.getId());
+
+		// la date de création est utilisée pour définir le mois affiché (sinon par
+		// défaut il affiche le mois en cours)
 		arCalendarVM.setCreatedAt(ar.getCreatedAt());
+		
+		// on récupère la liste des activityDate
 		arCalendarVM.setActivityDates(activityDateRepo.getAllActivityDateByArId(ar.getId()));
 		arCalendarVM.setUpdatedAt(ar.getUpdatedAt());
 		arCalendarVM.setStateAr(ar.getStateArEnum());
+		
+		// chaque activityDate de la liste est ajouté comme event au calendrier
 		List<ActivityDate> activityDatesAsEvents = arCalendarVM.getActivityDates();
 		for (ActivityDate activityDateAsEvent : activityDatesAsEvents) {
-			DefaultScheduleEvent<?> event2 = DefaultScheduleEvent.builder().title("test")
-					.startDate(activityDateAsEvent.getDate().atTime(9, 0))
-					.endDate(activityDateAsEvent.getDate().atTime(18, 0)).build();
-			calendar.addEvent(event2);
+			//création de l'event en fonction de la valeur de PartOfDay
+			if (activityDateAsEvent.getPartOfDay() == PartDayEnum.MORNING) {
+				DefaultScheduleEvent<?> event = DefaultScheduleEvent.builder()
+						.title(activityDateRepo.getActivityLabelFromActivityDate(activityDateAsEvent.getId()))
+						.startDate(activityDateAsEvent.getDate().atTime(9, 0))
+						.endDate(activityDateAsEvent.getDate().atTime(13, 0)).build();
+				calendar.addEvent(event);
+			} else if (activityDateAsEvent.getPartOfDay() == PartDayEnum.AFTERNOON) {
+				DefaultScheduleEvent<?> event = DefaultScheduleEvent.builder()
+						.title(activityDateRepo.getActivityLabelFromActivityDate(activityDateAsEvent.getId()))
+						.startDate(activityDateAsEvent.getDate().atTime(14, 0))
+						.endDate(activityDateAsEvent.getDate().atTime(18, 0)).build();
+				calendar.addEvent(event);
+			}else {
+				DefaultScheduleEvent<?> event = DefaultScheduleEvent.builder()
+						.title(activityDateRepo.getActivityLabelFromActivityDate(activityDateAsEvent.getId()))
+						.startDate(activityDateAsEvent.getDate().atTime(9, 0))
+						.endDate(activityDateAsEvent.getDate().atTime(18, 0)).build();
+				calendar.addEvent(event);
+			}
 		}
 		return "ShowArAsCalendar.xhtml";
 	}
 
+	//Getters & setters
 	public ArCalendarViewModel getArCalendarVM() {
 		return arCalendarVM;
 	}

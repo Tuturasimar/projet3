e70e981a33a68ee80b8e63ff1436aad2d6743f04
arbam5 <arbam.saine@gmail.com>
2023-06-08 12:03:47 +0200

    HomePage

---
+++ Actiview/actiview-javaee-client-app/src/main/java/fr/isika/cda/beans/imagesView.java
@@ -0,0 +1,27 @@
+package fr.isika.cda.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class imagesView {
+
+    private List<String> images;
+
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 12; i++) {
+            images.add("image" + i + ".jpg");
+        }
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+}
